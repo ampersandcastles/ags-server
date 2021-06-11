ac4ecb15f113855f65d7eaa27c7815aee11c9f51
chels_s <clsextonc734@gmail.com>
2021-06-11 14:14:09 -0400

    fixed minor error in db.js

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
-const { Sequelize } = require('sequelize/types');
-
 require ('dotenv').config();
 
-
-const sequelize = new Sequelize(proncess.env.DB_CONNECTION_STRING, {
-    dialect: 'postgres'.
-})
-
-// const {Sequelize} = require('sequelize');
-// const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+const {Sequelize} = require('sequelize');
+const db = new Sequelize(process.env.DB_CONNECTION_STRING);
 
 module.exports = db;
