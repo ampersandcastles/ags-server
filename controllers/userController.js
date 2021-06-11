255b228ff928f16a21b97e05aa0fc22bf795c122
chels_s <clsextonc734@gmail.com>
2021-06-11 10:34:28 -0400

    Created index.js for controller, models, middleware. Updated db, controllers, model and middleware to connect to local database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,53 @@
-let express = require('express');
-let router = express.Router();
-
-const User = require('../db').import('../models/user');
+const {UniqueConstraintError} = require('sequelize');
+const router = require('express').Router();
+const {UserModel} = require('../models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+
+
 
 /**********************
  * USER - SIGNUP
  ****************/
-router.post('/signup', function(req,res){
-    User.create({
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 13)
+router.post('/signup', async (req,res) => {
+    const {firstName, lastName, email, password} = req.body;
+
+    try {
+        const newUser = await UserModel.create({
+            firstName,
+            lastName,
+            email,
+            password: bcrypt.hashSync(password,13)
+        })
+    
+
+    const token = jwt.sign(
+        {id: newUser.id},
+        process.env.JWT_SECRET,
+        {expiresIn: 60*60*24}
+    )
+
+    res.status(201).json({
+        message: "User has been registered.",
+        user: newUser,
+        token
     })
 
-    .then( user => {
-
-        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
-
-        res.status(200).json({
-            user: user,
-            message: "New user has been created.",
-            sessionToken: token
+} catch (error) {
+    if (error instanceof UniqueConstraintError){
+        res.status(409).json({
+            message: `That email is already in use.`
         });
-    })
-    .catch(err => res.status(500).json({error :err}))
-});
+    }else {
+        res.status(500).json({
+            error: `Failed to register user: ${error}`
+        })
+    }
+}
+})
+
+
+
 
 
 
@@ -33,37 +55,37 @@ router.post('/signup', function(req,res){
  /*****************
  * USER - LOGIN
  ****************/
-router.post('/login', function(req,res){
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
+// router.post('/login', function(req,res){
+//     User.findOne({
+//         where: {
+//             email: req.body.email
+//         }
+//     })
 
-    .then(user => {
-        if(user){
+//     .then(user => {
+//         if(user){
 
-            bcrypt.compare(req.body.password, user.password, function(err,matches) {
+//             bcrypt.compare(req.body.password, user.password, function(err,matches) {
 
-                if(matches){
-                    let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+//                 if(matches){
+//                     let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
 
-                    res.status(200).json({
-                        user: user,
-                        message: "User is logged in.",
-                        sessionToken: token
-                    });
-                } else {
-                    res.status(502).send({error: "Login failed" });
-                }
-            });
-        } else {
-            res.status(500).json({error: 'User does not exist.'})
-        }
-    })
-    .catch(err => res.status(500).json({error :err}))
+//                     res.status(200).json({
+//                         user: user,
+//                         message: "User is logged in.",
+//                         sessionToken: token
+//                     });
+//                 } else {
+//                     res.status(502).send({error: "Login failed" });
+//                 }
+//             });
+//         } else {
+//             res.status(500).json({error: 'User does not exist.'})
+//         }
+//     })
+//     .catch(err => res.status(500).json({error :err}))
 
-});
+// });
 
 
 
