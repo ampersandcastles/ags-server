e29a6acf14aced3f0e47cd585a5b2f9d5af72bad
Chelsey <clsextonc734@gmail.com>
2021-06-13 00:44:22 -0400

    ran tests for get, post, put, delete routes for the product controller and updated accordingly

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 const {ProductModel} = require('../models');
-const middleware = require('../middleware');
 
 let validateSession = require("../middleware/validateSession");
-const {ProductModel} = require('../models');
+
 
 
 /*********************
- * PRODUCT - CREATE A NEW ITEM TO BE ADDED TO DATABASE
+ * PRODUCT - CREATE A NEW PRODUCT TO BE ADDED TO DATABASE
  ********************/
 router.post('/', validateSession, async (req, res) => {
     const {nameOfProduct, availability, 
@@ -22,119 +21,150 @@ router.post('/', validateSession, async (req, res) => {
             message: "Product successfully created.",
             product
         })
-    }catch(err){
+    }catch(error){
         res.status(500).json({
-            error: err
+            error: `Failed to created product: ${error}`
         })
     }
 });
 
 /*********************
- * PRODUCT - DELETE INDIVIDUAL LOGS BY USER
+ * PRODUCT - GETS ALL PRODUCTS FOR USER
  ********************/
- router.delete("/delete/:id", validateSession, function (req, res) {
-    const query = { where: { id: req.params.id, owner: req.user.id } };
-  
-    Product.destroy(query)
-      .then(() => res.status(200).json({ message: "Product Entry Removed" }))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
-  
-
-/*********************
- * PRODUCT - GETS ALL ITEMS FOR USER
- ********************/
-router.get("/", (req, res) => {
-  Product.findAll()
-    .then((products) => res.status(200).json(products))
-    .catch((err) => res.status(500).json({ error: err }));
+ router.get("/all", validateSession, async (req, res) => {
+    try {
+        const allProdcuts = await ProductModel.findAll();
+        res.status(200).json(allProdcuts)
+    }catch(error){
+        res.status(500).json({
+            error: `Cannot find products: ${error}`
+        })
+    }
 });
-
-
 
 
 /************************
- * PRODUCT - GET BY NAME
+ * PRODUCT - GET All PRODUCTS BY TYPE
  ***********************/
-router.get("/:name", function (req, res) {
-  let name = req.params.name;
+ router.get("/:type", validateSession, async (req, res) => {
+    try {
+        const specificProduct = await ProductModel.findAll({
+            where: {type: req.params.type}
+        })
+        res.status(200).json({
+            message: `Product successfully found.`,
+            product: specificProduct
+        })
+    }catch (error){
+        res.status(500).json({
+            message: `Failed to find products: ${error}`
+        })
+    }
+ })
 
-  Product.findAll({
-    where: { name: name },
-  })
-    .then((products) => res.status(200).json(products))
-    .catch((err) => res.status(500).json({ error: err }));
-});
+
+
+
+/*********************
+ * PRODUCT - UPDATE INDIVIDUAL PRODUCT
+ ********************/
+ router.put("/edit/:id", validateSession, async (req, res) => {
+    const {nameOfProduct, availability, 
+        description, type, price, photoURL} = req.body;
+
+    try {
+        const productUpdate = await ProductModel.update({nameOfProduct, availability, 
+            description, type, price, photoURL},
+            {where:{id: req.params.id}}
+        )
+        res.status(200).json({
+            message: "Product successfully updated",
+            productUpdate
+        })
+    }catch(error) {
+        res.status(500).json({
+            message: `Failed to update product: ${error}`
+        })
+    }
+
+  });
+
+
+
+/*********************
+ * PRODUCT - DELETE INDIVIDUAL PRODUCT 
+ ********************/
+
+ router.delete("/delete/:id", validateSession, async (req, res) => {
+    try {
+        const deletedProduct = await ProductModel.destroy({
+            where: {id: req.params.id}
+        })
+        res.status(200).json({
+            message: "Product has been successfully deleted",
+            deletedProduct
+        })
+    }catch (error) {
+        res.status(500).json({
+            message: `Failed to delete product: ${error}`
+        })
+    }
+  });
+  
+
+
 
 
 
 /*********************
  * PRODUCT - GET BY CATEGORY
  ********************/
- router.get('/category/:category', (req,res) => {
-    Product.findAll({
-        where: {
-            category: req.params.category
-        }
-    })
-        .then(logs => res.status(200).json({
-            logs: logs
-        }))
-        .catch(err => res.status(500).json({
-            error: err
-        }))
-});
-
-/*********************
- * PRODUCT - UPDATE
- ********************/
-router.put("/edit/:id", validateSession, function (req, res) {
-  const updateProductEntry = {
-    name: req.body.product.name,
-    price: req.body.product.price,
-    description: req.body.product.description,
-    availability: req.body.product.availability,
-    photoURL: req.body.product.photoURL,
-  };
-  const query = { where: { id: req.params.id, owner: req.user.id } };
-
-  Product.update(updateProductEntry, query)
-    .then((products) => res.status(200).json(products))
-    .catch((err) => res.status(500).json({ error: err }));
-});
+//  router.get('/category/:category', (req,res) => {
+//     Product.findAll({
+//         where: {
+//             category: req.params.category
+//         }
+//     })
+//         .then(logs => res.status(200).json({
+//             logs: logs
+//         }))
+//         .catch(err => res.status(500).json({
+//             error: err
+//         }))
+// });
 
 /*********************
  * GET PRODUCT - INDIVIDUAL ITEMS FOR A USER
  ********************/
-router.get('/:id', (req,res) => {
-    Food.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(log => res.status(200).json({
-        log: log
-    }))
-    .catch(err => res.status(500).json({
-        error: err
-    }))
-});
+// router.get('/:id', (req,res) => {
+//     Food.findOne({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//     .then(log => res.status(200).json({
+//         log: log
+//     }))
+//     .catch(err => res.status(500).json({
+//         error: err
+//     }))
+// });
 
 /*********************
  * PUT -ALLOWS LOGGED ITEMS TO BE UPDATED BY USER
  ********************/
-router.put('/:id', (req, res) => {
-    Product.update(req.body, {
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(log => res.status(200).json({
-        log: log
-    }))
-    .catch(err => res.status(500).json({
-        error: err
-    }))
-});
+// router.put('/:id', (req, res) => {
+//     Product.update(req.body, {
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//     .then(log => res.status(200).json({
+//         log: log
+//     }))
+//     .catch(err => res.status(500).json({
+//         error: err
+//     }))
+// });
 
 module.exports = router;
--- db.js
+++ db.js
@@ -3,8 +3,4 @@ require ('dotenv').config();
 const {Sequelize} = require('sequelize');
 const db = new Sequelize(process.env.DB_CONNECTION_STRING);
 
-<<<<<<< HEAD
 module.exports = db;
-=======
-module.exports = db;
->>>>>>> cd305f7 (testing edits)
